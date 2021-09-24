be8659e7702e0ac771b1105ff495ce00a06edb2c
alexsandroferreira <ferreira.alex@outlook.com>
2021-09-23 22:22:53 -0300

    components

---
+++ pages/css/integration.jsx
@@ -0,0 +1,8 @@
+export default function integration(){
+    return(
+        <div>
+            <div className="red">testo 01</div>
+            <div className="blue">texto 02</div>
+        </div>
+    )
+}
---
+++ pages/css/integration2.jsx
@@ -0,0 +1,9 @@
+import styles from "./integration2.module.css"
+export default function integration2(){
+    return(
+        <div>
+            <div className={styles.blue}>testo 01</div>
+            <div className={styles.red} >texto 02</div>
+        </div>
+    )
+}
